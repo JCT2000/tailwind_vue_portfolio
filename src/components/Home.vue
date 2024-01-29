f1926bcb61513408090056e454bc3738b7b8459f
John Christian Triana <trianajohnchristian@gmail.com>
2024-01-29 23:16:46 +0800

    update

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Hello World!</h1>
+    <img src="../assets/images/H254K00994.JPG" class="w-[200px] h-[255px]" alt="mine" />
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
